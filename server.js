ade21649a05b2b8d0414bd719916e25183bd0cf1
Jesse-Tingle <jtingle0@gmail.com>
2020-03-15 00:44:34 -0400

    create getRecipes for recipes-model file and GET route for all recipes in recipes-router file

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
+const RecipesRouter = require("./recipes/recipes-router.js");
+
 const server = express();
 
 server.use(express.json());
+server.use("/api/recipes", RecipesRouter);
 
 server.get("/", (req, res) => {
 	res.send(`<h2>DB 4 Project!</h2>`);
